2a4a6fca0c6fd68bbe0f80053906470c61196fa5
siyaajoshi <siyaj52@gmail.com>
2025-01-17 00:19:17 -0500

    trial

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./containers";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create the root
 root.render(
   <React.StrictMode>
     <App />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scans your src directory for classes
+  safelist: [ // Move safelist outside of `theme`
+    "bg-primary",
+    "text-secondary",
+    "hover:bg-gradient-to-br",
+    "from-primary",
+    "to-secondary",
+    "bg-bgPrimary",
+  ],
   theme: {
     extend: {
       colors: {
         primary: "#FF5733",
         secondary: "#FF5733",
         texlight: "#a7a7a7",
-        bgPrimary: "#020010",
+        bgPrimary: "#020010", // Black background color
       },
     },
   },
