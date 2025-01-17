3167c3f9cee682751b41d33ef477665e9774e12f
siyaajoshi <siyaj52@gmail.com>
2025-01-17 00:50:08 -0500

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as SiyaTypeWritter } from "./SiyaTypeWritter";
-export { default as HomeSocialLinks } from "./HomeSocialLinks";
-export { default as MenuItem } from "./MenuItem";
-export { default as ServiceCard } from "./ServiceCard";
-export { default as SkillCard } from "./SkillCard";
